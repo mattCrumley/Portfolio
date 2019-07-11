30c2e490c51ef571e4ef4db2697d5c6802738ba5
Matthew Crumley <mcrumley86@gmail.com>
2019-07-11 09:12:30 -0500

    Initial commit
    Completed:
    Overall site design
    
    Still need:
    A better cover page photo?
    
    A better portrait photo.
    
    About Me section updated with skills/more about me
    
    projects
    
    Links to social media sites (github,fb,linkedin) should be updated

---
+++ portfolio.js
@@ -0,0 +1,38 @@
+var aboutMe = document.querySelector("#aboutMe");
+var aboutMeButton = document.querySelectorAll(".aboutMeButton");
+var projects = document.querySelector("#projects");
+var projectsButton = document.querySelectorAll(".projectsButton");
+
+var contactMeButton = document.querySelectorAll(".contactMeButton");
+
+aboutMe.addEventListener("mouseover",function(){
+	aboutMe.style.opacity = 1;
+});
+aboutMe.addEventListener("mouseout",function(){
+	aboutMe.style.opacity = 0.4;
+});
+
+projects.addEventListener("mouseover",function(){
+	projects.style.opacity = 1;
+});
+		
+projects.addEventListener("mouseout",function(){
+	projects.style.opacity = 0.4;
+});
+
+
+
+for(var i = 0; i < aboutMeButton.length; i++){
+	aboutMeButton[i].addEventListener("click", function(){
+		aboutMe.style.opacity = 0.4;
+		
+	});
+}
+
+for(var i = 0; i < projectsButton.length; i++){
+	projectsButton[i].addEventListener("click", function(){
+		projects.style.opacity = 0.4;
+		
+	});
+}
+
