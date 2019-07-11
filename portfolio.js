43a44b8a696efe9f05c8c8e3fdeefb2364226175
Matthew Crumley <mcrumley86@gmail.com>
2019-07-11 09:35:28 -0500

    indenting
    Updated indenting to reflect html/css and javascript styleguide

--- portfolio.js
+++ portfolio.js
@@ -2,12 +2,12 @@ var aboutMe = document.querySelector("#aboutMe");
 var aboutMeButton = document.querySelectorAll(".aboutMeButton");
 var projects = document.querySelector("#projects");
 var projectsButton = document.querySelectorAll(".projectsButton");
-
 var contactMeButton = document.querySelectorAll(".contactMeButton");
 
 aboutMe.addEventListener("mouseover",function(){
 	aboutMe.style.opacity = 1;
 });
+
 aboutMe.addEventListener("mouseout",function(){
 	aboutMe.style.opacity = 0.4;
 });
@@ -20,19 +20,15 @@ projects.addEventListener("mouseout",function(){
 	projects.style.opacity = 0.4;
 });
 
-
-
 for(var i = 0; i < aboutMeButton.length; i++){
 	aboutMeButton[i].addEventListener("click", function(){
 		aboutMe.style.opacity = 0.4;
-		
 	});
 }
 
 for(var i = 0; i < projectsButton.length; i++){
 	projectsButton[i].addEventListener("click", function(){
-		projects.style.opacity = 0.4;
-		
+		projects.style.opacity = 0.4;	
 	});
 }
 
